698798fc62e908c3dcb7b80a16334622f5e2aac3
Guadalupe Isidro (LÜ ANGELES) <51386526+LuAngeles@users.noreply.github.com>
2019-06-05 15:55:36 -0500

    últimos cambios
    Hice cambios en TRIVIA_SCRIPT

--- TRIVIA_SCRIPT.js
+++ TRIVIA_SCRIPT.js
@@ -1,4 +1,17 @@
-//Preguntar nombre al usuario.
+/*//Preguntar nombre al usuario.
 var usuario = prompt ("¿Cómo te llamas?");
 //Saludar al usuario con su nombre.
 document.write ("¡Hola " + usuario + "!");
+*/
+//Se cambia el prompt con el que se pedía el nombre, por una función//
+
+function escribe(){
+    escribir= document.getElementById('caja')
+    miNombre= "<h2>" + document.rellenar.nombre.value +"</h2>"
+   
+    escribir.innerHTML= miNombre
+   }
+   
+   window.onload= function () {
+    document.rellenar.ver.onclick =escribe
+   }
